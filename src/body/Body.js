c80a043bfd77c4d023d185b73d66eb2353f74a88
Hendrix Roa <hroa@master.inmobilia.com>
2017-05-27 23:07:12 -0400

    Split master component default by components child

---
+++ src/body/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Body.css';
+
+class Body extends Component {
+  render() {
+    return (
+      <p className="Body-intro">
+          Your prefer site e-wallet 
+      </p>
+    );
+  }
+}
+
+export default Body;
---
+++ src/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h2>Welcome to React</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
